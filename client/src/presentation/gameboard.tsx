eee2e219330256ca827a72700d34857e76fa69cf
jhughs26 <jared.hughes@e-hps.com>
2023-11-10 16:26:29 -0600

    Feature: gameboard initialized and rendered (WIP)

--- client/src/presentation/gameboard.tsx
+++ client/src/presentation/gameboard.tsx
@@ -1,5 +1,14 @@
+import { useEffect } from "react"
+
 function Gameboard() {
-    return (<h1>hello gameboard</h1>)
+    useEffect(()=> {
+        const canvas=document.getElementById("gameboard")
+        const context=canvas.getContext("2d")
+        context.fillStyle="purple"
+        context.fillRect(10,10,150,100) 
+    })
+    
+    return (<canvas id="gameboard"/>)
 }
 
 export {Gameboard}
