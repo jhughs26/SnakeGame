c2d2a24af14925b833643fb64be34b2fd6698bc9
Darren Jaworski <darren.jaworski@e-hps.com>
2023-11-11 09:33:14 -0600

    chore: prettier format code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/bigRocket.png'
-import snakeLogo from './assets/Snake.png'
-import { Gameboard } from './presentation/gameboard'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/bigRocket.png";
+import snakeLogo from "./assets/Snake.png";
+import { Gameboard } from "./presentation/gameboard";
+import "./App.css";
 
 function App() {
-  const [showgame, setShowgame] = useState(false)
+  const [showgame, setShowgame] = useState(false);
 
   if (showgame) {
-    return(<Gameboard />)
+    return <Gameboard />;
   }
 
   return (
@@ -21,15 +21,13 @@ function App() {
       <h1>Big Rocket Gaming</h1>
       <h2>presents</h2>
       <a>
-          <img src={snakeLogo} className="snakeLogo" alt="Snake logo" />
-        </a>
-      <div className="snakeLogo"> 
-        <button onClick={() => setShowgame(true)}>
-          Start Game
-        </button>
+        <img src={snakeLogo} className="snakeLogo" alt="Snake logo" />
+      </a>
+      <div className="snakeLogo">
+        <button onClick={() => setShowgame(true)}>Start Game</button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/presentation/gameboard.tsx
+++ client/src/presentation/gameboard.tsx
@@ -1,14 +1,14 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
 
 function Gameboard() {
-    useEffect(()=> {
-        const canvas=document.getElementById("gameboard")
-        const context=canvas.getContext("2d")
-        context.fillStyle="purple"
-        context.fillRect(10,10,150,100) 
-    })
-    
-    return (<canvas id="gameboard"/>)
+  useEffect(() => {
+    const canvas = document.getElementById("gameboard");
+    const context = canvas.getContext("2d");
+    context.fillStyle = "purple";
+    context.fillRect(10, 10, 150, 100);
+  });
+
+  return <canvas id="gameboard" />;
 }
 
-export {Gameboard}
+export { Gameboard };
